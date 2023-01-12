c3565ad36c5dbf45383b8e205ca28d3b31823e9b
kenneth4832@gmail.com <kenneth4832@gmail.com>
2023-01-12 16:53:18 -0600

    Side menu update, new favicon, added README.md

--- src/components/PlaylistMenu/PlaylistMenu.jsx
+++ src/components/PlaylistMenu/PlaylistMenu.jsx
@@ -1,17 +1,57 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+
 import { SiApplemusic as AppLogo } from 'react-icons/si'
 import { BiSun as Sun, BiMoon as Moon } from 'react-icons/bi'
-import { BsGithub as Git } from 'react-icons/bs'
+import { BsGithub as GitHub } from 'react-icons/bs'
 import { SlMagnifier as Search } from 'react-icons/sl'
 import { HiChevronRight as Open, HiChevronDown as Close } from 'react-icons/hi'
 import { MdOutlineAdd as Add } from 'react-icons/md'
-import { RiFolderMusicLine as PLIcon } from 'react-icons/ri'
+
+import PlaylistMenuItem from '../PlaylistMenuItem/PlaylistMenuItem'
+import PlaylistMenuItemSettings from '../PlaylistMenuItemSettings/PlaylistMenuItemSettings'
 import './PlaylistMenu.css'
 
 const PlaylistMenu = () => {
   const [darkTheme, setDarkTheme] = useState(false)
-  const [playlistsOpen, setPlaylistsOpen] = useState(false)
+  const [dropdownOpened, setDropdownOpened] = useState(false)
+  const [totalPlaylists, setTotalPlaylists] = useState([])
+  const [menuCoordinates, setMenuCoordinates] = useState({x: 0, y: 0})
+  const [enableRenameMode, setEnableRenameMode] = useState(false)
+  const [renameRequestID, setRenameRequestID] = useState("")
+  const targetPlaylistRef = useRef("")
+  
+  const handleSearch = (e) => setDropdownOpened(e.target.value != '' ? true : dropdownOpened)
 
+  const renameDuplicate = (name) => {
+    let checkCount = 0
+    let baseName = name
+    let newName = ""
+    while(totalPlaylists.includes(name))
+      name = `${baseName} (${checkCount+=1})`
+    newName = name
+    return newName == '' ? name : newName
+  }
+
+  const positionContextMenu = (xPos, yPos, ref) => {
+    targetPlaylistRef.current = ref.current
+    clearRenameRequestID()
+    setMenuCoordinates({x: xPos, y: yPos})
+  }
+
+  const getRenameRequest = (reqID) => {
+    setEnableRenameMode(true)
+    setRenameRequestID(reqID)
+    console.log("Rename request for:", reqID, "\nRename mode enabled.")
+  }
+
+  const replaceOldPlaylistName = (oldName, newName) => {
+    let allNames = totalPlaylists
+    allNames[allNames.indexOf(oldName)] = newName
+    setTotalPlaylists(allNames)
+  }
+
+  const clearRenameRequestID = () => setRenameRequestID("")
+  
   return (
     <aside className="playlistMenu">
       <div className="appName">
@@ -19,6 +59,10 @@ const PlaylistMenu = () => {
         Tunes
       </div>
       <div className="appOptionSection">
+        <a href="https://github.com/KennethOnuorah/React-Tunes?" target="_blank" rel="noopener noreferrer">
+          <GitHub color='lightgrey' size={20}/>
+          Visit Repository
+        </a>
         <button onClick={() => setDarkTheme(!darkTheme)}>
           {
           !darkTheme ? 
@@ -27,32 +71,49 @@ const PlaylistMenu = () => {
           }
           Set {!darkTheme ? "Dark" : "Light"} Theme
         </button>
-        <button>
-          <Git color='lightgrey' size={20}/>
-          Visit Repository
-        </button>
       </div>
       <hr/>
       <div className="playlistController">
         <div className="searchPlaylists">
           <Search size={20}/>
-          <input type="search" placeholder='Search playlists . . .'/>
+          <input type="search" placeholder='Search playlists . . .' onInput={handleSearch}/>
         </div>
         <div className="createPlaylistBtn">
           <div className="playlistDropdown">
-            <button onClick={() => setPlaylistsOpen(!playlistsOpen)}>
-              {
-              !playlistsOpen ? 
+            <button onClick={() => setDropdownOpened(!dropdownOpened)}>
+              {!dropdownOpened ? 
                 <Open size={20} color='lightgrey'/> : 
-                <Close size={20} color='lightgrey'/>
-              }
+                <Close size={20} color='lightgrey'/>}
             </button>
             Playlists
           </div>
-          <button>
+          <button 
+            title='Create new playlist'
+            onClick={() => {
+              setDropdownOpened(true)
+              setTotalPlaylists([renameDuplicate("New playlist"), ...totalPlaylists])
+            }}>
             <Add size={20} color='lightgrey'/>
           </button>
         </div>
+      </div>
+      <div className="playlistContainer">
+        {totalPlaylists.map((name) => dropdownOpened && 
+          <PlaylistMenuItem
+            key={`${name}_${Math.ceil(Math.pow(10, 10) * Math.random() * Math.random())}`} 
+            name={name}
+            handleContextMenu={positionContextMenu}
+            enableRenameMode={renameRequestID === "pl_"+name ? enableRenameMode : false}
+            replaceOldPlaylistName={replaceOldPlaylistName}
+            clearRenameRequestID={clearRenameRequestID}
+            renameDuplicate={renameDuplicate}
+          />)}
+        <PlaylistMenuItemSettings 
+          xPos={menuCoordinates.x}
+          yPos={menuCoordinates.y} 
+          targetElementID={targetPlaylistRef.current !== '' ? targetPlaylistRef.current.id : ""}
+          getRenameRequest={getRenameRequest}
+        />
       </div>
     </aside>
   )
